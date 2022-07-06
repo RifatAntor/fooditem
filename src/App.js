2432dc4036dd518646656e43045a7983c9b5818a
RifatAntor <himuami3@gmail.com>
2022-07-07 04:21:04 +0600

    upload project....

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import Axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+import './App.css';
+import RecipeComponent from "./components/RecipeComponent ";
+
+const APP_ID = "91b7dc6a";
+const APP_KEY = "563178b3f188ac942c19e6ce06fb911f	";
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+
+
+const Placeholder = styled.span`
+font-size: 18px;
+font-weight: 600;
+color: black;
+margin: 10px 0;
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const RecipeListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 20px;
+  justify-content: space-evenly;
+`;
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [recipeList, updateRecipeList] = useState([]);
+  const [timeoutId, updateTimeoutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+    );
+    updateRecipeList(response.data.hits);
+  };
+
+  const onTextChange = (e) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+  return (
+    <Container>
+      <Header>
+        <AppName>
+          FOOD ITEM
+        </AppName>
+        <SearchBox>
+          
+          <SearchInput
+            placeholder="Search FOOD ITEM"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      <RecipeListContainer>
+        {recipeList?.length ? (
+          recipeList.map((recipe, index) => (
+            <RecipeComponent key={index} recipe={recipe.recipe} />
+          ))
+        ) : (
+          <Placeholder>Food item</Placeholder>
+        )}
+      </RecipeListContainer>
+    </Container>
+  );
+}
+
+export default App;
+
